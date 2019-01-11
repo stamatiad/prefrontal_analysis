790664043fdabeb3b99973ea0f30e57edb02afb8
stevest <stamatiad.st@gmail.com>
2019-01-11 17:56:37 +0200

    Implement mean-shift algorithm.

--- analysis.py
+++ analysis.py
@@ -1,30 +1,25 @@
-from analysis_tools import quick_spikes
-from analysis_tools import q_generator
+#from analysis_tools import quick_spikes
+#from analysis_tools import generate_slices_g
+import analysis_tools as at
+from analysis_tools import from_zero_to, from_one_to
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from pathlib import Path
 import pandas as pd
 import h5py
-from contextlib import contextmanager
 from sklearn import decomposition
 from mpl_toolkits.mplot3d import Axes3D
+from collections import namedtuple
+from scipy import spatial
 
-'''
-@contextmanager
-def open_hdf_dataframe(filename=None, hdf_key=None):
-    try:
-        yield pd.read_hdf(filename, key=hdf_key)
-    except Exception:
-        print(f'File ({filename}) or key ({hdf_key}) not found!')
-        yield
-    finally:
-        pass
-    pass
-'''
-window_size = 50
-total_qs = int(np.floor(3000 / window_size))
-ntrials = 10
+# Quick class, containing the properties of the analysis:
+Properties = namedtuple('Properties', [
+    'sim_stop',
+    'q_size',
+    'total_qs',
+    'ntrials'
+])
 
 class open_hdf_dataframe():
 
@@ -41,116 +36,158 @@ class open_hdf_dataframe():
         pass
     pass
 
-def voltage_to_network_activity():
+experiment_config_str = 'SN{animal_model}LC{learning_condition}'.format
+
+simulation_template = (
+    'SN{animal_model}'
+    'LC{learning_condition}'
+    'TR{trial}'
+    '_EB1.750'
+    '_IB1.500'
+    '_GBF2.000'
+    '_NMDAb6.000'
+    '_AMPAb1.000'
+    '_randomdur3').format
+
+def simulation_to_network_activity(tofile=None, animal_models=None, learning_conditions=None, properties=None):
     # Convert voltage traces to spike trains:
     cellno = 333
     upper_threshold = 0
     lower_threshold = -10
 
-    simulation_template = (
-        'SN{animal_model}'
-        'LC{learning_condition}'
-        'TR{trial}'
-        '_EB1.750'
-        '_IB1.500'
-        '_GBF2.000'
-        '_NMDAb6.000'
-        '_AMPAb1.000'
-        '_randomdur3').format
-
     # For multiple runs:
     # Use panda dataframes to keep track across learning conditions and animal models.
     # Group multiple trials in the same dataframe, since these are our working unit.
-    filename_hdf5 = Path(fr'G:\Glia\all_data.hdf5')
-    open(filename_hdf5, 'w').close()
+    # Touch the output file:
+    open(tofile, 'w').close()
 
-    animal_models = range(1, 4 + 1)
-    learning_conditions = range(1, 10 + 1)
-    trials = range(10)
 
     for animal_model in animal_models:
         for learning_condition in learning_conditions:
-            windowed_activity = np.zeros((ntrials, total_qs, cellno), dtype=int)
-            data_key = f'SN{animal_model}LC{learning_condition}'
-            print(f'Handling: {data_key}')
-            for trial in trials:
-                filename = Path('G:\Glia').joinpath(
+            windowed_activity = np.zeros((properties.ntrials, properties.total_qs, cellno), dtype=int)
+            #data_key = f'SN{animal_model}LC{learning_condition}'
+            dataset = experiment_config_str(
+                animal_model=animal_model,
+                learning_condition=learning_condition
+            )
+            print(f'Handling: {dataset}')
+            for trial in range(properties.ntrials):
+                inputfile = Path('G:\Glia')\
+                    .joinpath(
                     simulation_template(animal_model=animal_model,
                                         learning_condition=learning_condition,
-                                        trial=trial)
-                ).joinpath('vsoma.hdf5')
-
-                #df = pd.read_hdf(Path('G:\Glia').joinpath(filename).joinpath('vsoma.hdf5'), key='vsoma')
-                with open_hdf_dataframe(filename=filename, hdf_key='vsoma') as df:
-                    if df is not None:
-                        # Convert dataframe to ndarray:
-                        voltage_traces = df.values
-                        # Reduce each voltage trace to a list of spike times:
-                        spike_trains = [
-                            quick_spikes(voltage_trace=voltage_trace,
-                                         upper_threshold=upper_threshold,
-                                         lower_threshold=lower_threshold,
-                                         plot=False)
-                            for voltage_trace in voltage_traces
-                        ]
-                        # Sum spikes inside a window Q:
-                        for cellid, spike_train in enumerate(spike_trains):
-                            if len(spike_train) > 0:
-                                for q, (q_start, q_end) in enumerate(q_generator(q_size=window_size, q_total=total_qs)):
-                                    windowed_activity[trial][q][cellid] = sum([1 for spike in spike_train if q_start <= spike and spike < q_end])
-            #fig, ax = plt.subplots()
-            #ax.imshow(windowed_activity[trial])
-            #plt.show()
+                                        trial=trial))\
+                    .joinpath('vsoma.hdf5')
+                #with open_hdf_dataframe(filename=filename, hdf_key='vsoma') as df:
+                if inputfile.exists():
+                    # Convert dataframe to ndarray:
+                    voltage_traces = pd.read_hdf(inputfile, key='vsoma').values
+                    # Reduce each voltage trace to a list of spike times:
+                    spike_trains = [
+                        at.quick_spikes(voltage_trace=voltage_trace,
+                                     upper_threshold=upper_threshold,
+                                     lower_threshold=lower_threshold,
+                                     plot=False)
+                        for voltage_trace in voltage_traces
+                    ]
+                    # Sum spikes inside a window Q:
+                    for cellid, spike_train in enumerate(spike_trains):
+                        if len(spike_train) > 0:
+                            for q, (q_start, q_end) in enumerate(at.generate_slices_g(size=properties.q_size, number=properties.total_qs)):
+                                windowed_activity[trial][q][cellid] = sum([1 for spike in spike_train if q_start <= spike and spike < q_end])
             #fig, ax = plt.subplots()
             #ax.imshow(windowed_activity.reshape(total_qs * ntrials, cellno, order='C'))
             #plt.show()
-            df = pd.DataFrame(windowed_activity.reshape(total_qs * ntrials, cellno, order='C'))
-            df.to_hdf(filename_hdf5, key=data_key, mode='a')
+            df = pd.DataFrame(windowed_activity.reshape(properties.total_qs * properties.ntrials, cellno, order='C'))
+            df.to_hdf(tofile, key=dataset, mode='a')
 
 
 #===================================================================================================================
-def read_network_activity(filename=None, data_key=None, plot=False):
+def read_network_activity(fromfile=None, dataset=None, properties=None):
     data = None
     # Read spiketrains and plot them
-    with open_hdf_dataframe(filename=filename, hdf_key=data_key) as df:
-        if df is not None:
-            data = df.values
+    df = pd.read_hdf(fromfile, key=dataset)
+    #with open_hdf_dataframe(filename=filename, hdf_key=data_key) as df:
+    if df is not None:
+        data = df.values.T
+        # Also reshape the data into a 3d array:
+        data = data.reshape(data.shape[0], properties.ntrials, properties.total_qs, order='C')
+
     return data
 
-def pcaL2(data=None, plot=False):
+def pcaL2(data=None, plot=False, custom_range=None, properties=None):
     # how many components
     L = 2
     pca = decomposition.PCA(n_components=L)
-    t_L = pca.fit_transform(data).T
-    t_L_reshaped = t_L.reshape(L, ntrials, total_qs, order='C')
+    # Use custom_range to compute PCA only on a portion of the original data:
+    new_len = len(custom_range)
+    t_L = pca.fit_transform(
+        data[:250, :, custom_range].reshape(250, properties.ntrials * new_len).T
+    ).T
+    t_L_reshaped = t_L.reshape(L, properties.ntrials, new_len, order='C')
     if plot:
-        #fig, ax = plt.subplots()
-        #ax.imshow(data)
-        #plt.show()
         fig = plt.figure()
+        plt.ion()
         ax = fig.add_subplot(111, projection='3d')
-        for trial in range(ntrials):
-            ax.plot(t_L_reshaped[0][trial], t_L_reshaped[1][trial], range(total_qs))
+        colors = cm.viridis(np.linspace(0, 1, new_len - 1))
+        for trial in range(properties.ntrials):
+            for t, c in zip(range(new_len - 1), colors):
+                ax.plot(t_L_reshaped[0][trial][t:t+2], t_L_reshaped[1][trial][t:t+2], [t, t+1], color=c)
         plt.show()
+    return t_L_reshaped
 
 #===================================================================================================================
-# Do k-means in spiketrains:
 if __name__ == '__main__':
-    # Parse voltage traces into network activity matrices:
-    #voltage_to_network_activity()
+    # The file where network activity lies:
+    network_activity_hdf5 = Path(fr'G:\Glia\all_data.hdf5')
+
+    # Since namedtuple is immutable, pass arguments from a dictionary to make it easier on the eyes.
+    p_d = {
+        'sim_stop': 3000,
+        'q_size': 50,
+        'total_qs': None, # this must be calculated later on.
+        'ntrials': 10
+    }
+    p_d['total_qs'] = int(np.floor(p_d['sim_stop'] / p_d['q_size']))
+    p = Properties(**p_d)
+
+    # Parse voltage traces into network activity matrices once and save to file for later use:
+    '''
+    simulation_to_network_activity(
+        tofile=network_activity_hdf5,
+        animal_models=from_one_to(1),
+        learning_conditions=from_one_to(10),
+        ntrials=10,
+        properties=p
+    )
+    '''
 
     # Load and plot network activity:
-    filename_hdf5 = Path(fr'G:\Glia\all_data.hdf5')
-    animal_model = 1
-    learning_condition = 4
-    data_key = f'SN{animal_model}LC{learning_condition}'
-    data = read_network_activity(filename=filename_hdf5, data_key=data_key, plot=True)
+    # cellno, ntrials, total_qs
+    network_activity = read_network_activity(
+        fromfile=network_activity_hdf5,
+        dataset=experiment_config_str(
+            animal_model=1,
+            learning_condition=1
+        ),
+        properties=p
+    )
     #fig, ax = plt.subplots()
     #ax.imshow(data)
     #plt.show()
 
-    pcaL2(data=data, plot=True)
-
+    t_L = pcaL2(
+        data=network_activity,
+        custom_range=range(20, 60),
+        plot=False,
+        properties=p
+    )
 
     # k-means cluster network activity:
     #k_means_cluster_network_activity()
+    at.my_mean_shift(data=t_L, k=4, plot=True, properties=p, dims=2)
+    print('here!')
+
+
+
+
--- analysis_tools.py
+++ analysis_tools.py
@@ -4,6 +4,23 @@ from matplotlib import cm
 from pathlib import Path
 import pandas as pd
 import h5py
+from scipy import spatial
+
+def from_zero_to(x):
+    '''
+    Range wrapper, counting from zero to x excluded.
+    :param x:
+    :return:
+    '''
+    return range(x)
+
+def from_one_to(x):
+    '''
+    Range wrapper, counting from one to x included.
+    :param x:
+    :return:
+    '''
+    return range(1, x + 1)
 
 def quick_spikes(voltage_trace=None, upper_threshold=None, lower_threshold=None, steps_per_ms=10, plot=False):
     #   ADVANCED_SPIKE_COUNT(vt, lt, ht) find spikes in voltage trace vt ( that
@@ -56,19 +73,146 @@ def quick_spikes(voltage_trace=None, upper_threshold=None, lower_threshold=None,
 
     return spike_timings
 
-def q_generator(q_size=50, q_total=2):
+def generate_slices_g(size=50, number=2):
     '''
     Generate starting/ending positions of q_total windows q of size q_size.
     :param q_size:
     :param q_total:
     :return:
     '''
-    for q in range(1, q_total + 1):
-        q_start = (q - 1) * q_size + 1
-        q_end = (q - 1) * q_size + q_size
+    for q in from_one_to(number):
+        q_start = (q - 1) * size + 1
+        q_end = (q - 1) * size + size
         # yield starting/ending positions of q (in ms)
         yield (q_start, q_end)
         q += 1
+
+class NDPoint():
+    def __init__(self, ndarray=None):
+        assert ndarray.shape[0] == 1
+        self.ndarray = ndarray
+        self.m, self.n = ndarray.shape
+        # this is point dimension
+        self.dim = self.n
+
+
+class MeanShiftCentroid(NDPoint):
+    # This is specific to mean shift
+    @property
+    def shift(self):
+        return self._shift
+
+    @shift.setter
+    def shift(self, shift):
+        self._shift = shift
+
+    def __init__(self, ndarray=None):
+        NDPoint.__init__(self, ndarray)
+        self.shift = 100000
+
+    def distance_from(self, pts_array):
+        m, n = pts_array.shape
+        assert self.dim == n, 'Arrays have different dimensions'
+        distance = spatial.distance.pdist(
+            np.concatenate((self.ndarray, pts_array), axis=0)
+        )
+        return (distance[1:m+1]).reshape(m, 1)
+
+    def update(self, ndarray):
+        # Updates/moves centroid. Also calculates norm of movement
+        m, n = ndarray.shape
+        assert m == 1, 'Nd array must be row vector!'
+        self.shift = np.linalg.norm(self.ndarray - ndarray)
+        self.ndarray = ndarray
+
+
+
+
+
+def K_rbf(distance=None, sigma=None):
+    # Calculate K RBF for one or multiple points
+    # distance is ||x-x`||
+    return np.exp(-np.divide(np.power(distance, 2), 2 * (sigma ** 2)))
+
+def mu_bar(k_rbf=None, xs=None):
+    # calculate new mu_bar based on K_rbf:
+    # K can be np.ndarray of one or many scalars
+    m, n = xs.shape
+    m2, n2 = k_rbf.shape
+    assert m == m2, 'mu_bar: Arrays have different dims!'
+    x_mu_bar = np.multiply(k_rbf, xs).sum(axis=0) / k_rbf.sum()
+    return x_mu_bar.reshape(1,2)
+
+def my_mean_shift(data=None, k=None, plot=False, properties=None, dims = None):
+    #return [density_pts, sigma_hat]
+    # k for the kmeans (how many clusters)
+    # N are the number of trials (meanshift initial points)
+    N = properties.ntrials
+    # Collapse data to ndim points (2d):
+    new_len = data.shape[2]
+    pts = data.reshape(dims, properties.ntrials * new_len, order='C')
+    # Take the average STD the cells in PCA space:
+    sigma_hat = pts.std(axis=1)
+    # TODO: make slices smaller, we need a rational size window (1sec?) to account for activity drifting.
+    # TODO: to slicing den einai swsto gia C type arrays; prepei na to ftia3w, kai na bebaiw8w gia opou allou to xrisimopoiw!
+    std_array = np.array([
+        pts[:, slice_start:slice_end].std(axis=1)
+        for slice_start, slice_end in generate_slices_g(size=new_len, number=properties.ntrials)
+    ])
+    sigma_hat = std_array.mean(axis=0).mean()
+
+    if plot:
+        fig, ax = plt.subplots()
+        plt.ion()
+        ax.scatter(pts[0], pts[1], s=2, c='black')
+        plt.xlabel('Principal component 1')
+        plt.ylabel('Principal component 2')
+        plt.show()
+
+    # Initialize k centroids:
+    init_centroids = data.mean(axis=2)
+    centroids = [
+        MeanShiftCentroid(init_centroids[:, [x]].T)
+        for x in range(properties.ntrials)
+    ]
+    #rw_mus = squeeze(mean(X,1))'
+    while any([centroid.shift for centroid in centroids] > sigma_hat/10):
+        for centroid in centroids:
+            k_rbf = K_rbf(
+                distance=centroid.distance_from(pts.T),
+                sigma=sigma_hat
+            )
+            mu_x_bar = mu_bar(k_rbf=k_rbf, xs=pts.T)
+            #This centroid movement also updates the shift (movement distance):
+            if plot:
+                plt.plot(
+                    # TypeError: only integer scalar arrays can be converted to a scalar index. Why I get this:
+                    #np.concatenate(centroid.ndarray[0, [0]], mu_x_bar[0, [0]]),
+                    [centroid.ndarray[0, 0], mu_x_bar[0, 0]],
+                    [centroid.ndarray[0, 1], mu_x_bar[0, 1]],
+                    c='lime'
+                )
+                plt.scatter(centroid.ndarray[:,0], centroid.ndarray[:,1], s=20, c='lime')
+                plt.scatter(mu_x_bar[:,0], mu_x_bar[:,1], s=20, c='red', zorder=1000)
+                plt.pause(0.0001)
+            centroid.update(mu_x_bar)
+
+    print('done')
+
+
+def kmeans_clustering(data=None, initial_k=2, plot=False):
+    #return (labels_final, J_k_final, S_i)
+    # Perform kmeans clustering.
+    # Input:
+    #   X: n x d data matrix
+    #   m: initialization parameter (k)
+    # Adapted by Stefanos Stamatiadis (stamatiad.st@gmail.com).
+    # mahal_dist_flag = true
+    max_iter = 20
+    # wsize = 20 # 4*50=200ms window!
+    # n, d, N
+    dims, ntrials, total_qs  = data.shape
+    pass
 
 
 if __name__ == "__main__":
