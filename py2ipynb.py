5739a8ca5c8a5119161c1cf2404d5b97ff4b2ec1
stevest <stamatiad.st@gmail.com>
2019-06-12 20:41:28 +0300

    Compile Figures to ipynb format.

--- py2ipynb.py
+++ py2ipynb.py
@@ -1,4 +1,5 @@
 from nbformat import v3, v4
+from pathlib import Path
 
 '''
 This script converts python files to ipython notebooks. Since in the v4
@@ -10,15 +11,23 @@ https://github.com/jupyter/nbformat/blob/master/nbformat/v3/nbpy.py
 stamatiad.st@gmail.com 
 '''
 
-for id in range(1, 5):
-    with open(f'Generate_Publication_Figure_{id}.py') as python_file:
+figure_files = [
+    Path('Generate_Publication_Figure_1.py'),
+    Path('Generate_Publication_Figure_2.py'),
+    Path('Generate_Publication_Figure_3.py'),
+    Path('Generate_Publication_Figure_4.py'),
+    Path('Generate_Publication_Figure_S2.py'),
+    Path('Generate_Publication_Figure_S3.py'),
+]
+for fig_file in figure_files:
+    with open(fig_file) as python_file:
         python_code = python_file.read()
 
     notebook_v3 = v3.reads_py(python_code)
     notebook_v4 = v4.upgrade(notebook_v3)  # Upgrade v3 to v4
 
     jsonform = v4.writes(notebook_v4) + "\n"
-    with open(f'Generate_Publication_Figure_{id}.ipynb', "w") as fpout:
+    with open(Path(fig_file.stem).with_suffix('.ipynb'), "w") as fpout:
         fpout.write(jsonform)
 
 print('Done converting python files to ipython notebooks!')
